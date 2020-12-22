4cf9e8aae49f48debd5eabd543a65ebdf3442556
PhilipvEgmond <70648723+PhilipvEgmond@users.noreply.github.com>
2020-12-22 11:37:28 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,7 @@
-// -- Vars --
+// Homepage Animation 
 var container = document.querySelector("#homepage div");
 var img = document.querySelector("#homepage img");
 
-
-
-
-// -- Functions --
-
-// Homepage animation
 function animateImg(e) {
     let xAxis = (window.innerWidth / 2 - e.pageX) / -95;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 65;
@@ -22,13 +16,29 @@ function resetImg() {
     img.style.transform = `rotateX(0deg) rotateY(0deg)`;
 }
 
+// Takes away transition while animating
 function takeTransition() {
     img.style.transition = "none";
 }
 
-
-
-// -- Event Listeners --
 container.addEventListener("mousemove", animateImg);
 container.addEventListener("mouseleave", resetImg);
 container.addEventListener("mouseenter", takeTransition);
+
+
+
+// Footer animation
+var footer = document.querySelector("footer section:last-of-type");
+var footerH2 = document.querySelector("footer h2");
+
+window.onscroll = function(ev) {
+    var totalHeight = document.body.scrollHeight;
+    var scrollPoint = window.scrollY + window.innerHeight;
+
+    if(scrollPoint >= totalHeight - 260) {
+        footerH2.classList.remove("foot");
+    } else {
+        footerH2.classList.add("foot");
+    }
+};
+
